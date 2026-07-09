2f909101ea29890ece4839be86b105eb4d4f773b
morris <morrisx28@gmail.com>
2026-07-09 10:31:17 +0800

    add v3 config

--- src/pineapple_hardware/pineapple_sdk2_bridge.h
+++ src/pineapple_hardware/pineapple_sdk2_bridge.h
@@ -60,18 +60,18 @@ public:
     int set_zero_ = false;
     vector<uint16_t> can_id_list{0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08}; // L_hip L_thigh L_calf L_wheel R_hip R_thigh R_calf R_wheel
     vector<uint16_t> mst_id_list{0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18};
-    vector<int> motor_type{damiao::DM8006, damiao::DM8006, damiao::DM8009, damiao::DM6006, damiao::DM8006, damiao::DM8006, damiao::DM8009, damiao::DM6006};
+    vector<int> motor_type{damiao::DM4340, damiao::DM4340, damiao::DM4340, damiao::DM6006, damiao::DM4340, damiao::DM4340, damiao::DM4340, damiao::DM6006};
     
-    const vector<double> motor_offset{0.698, -1.919, 3.314, 0, -0.698, 1.919, -3.314, 0};
-    const vector<double> direction{-1.0, 1.0, 1.0, 1.0, -1.0, -1.0, -1.0, -1.0};
+    const vector<double> motor_offset{1.0472, -1.7453, 3.229, 0, -1.0472, -1.7453, 3.229, 0};
+    const vector<double> direction{1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
     const vector<MotorPosLimit> pos_limit_{
-        {-0.698, 0.698},
-        {-0.349, 1.919},
-        {-3.314, 0.174},
+        {-1.0472, 0.6108},
+        {0, 1.7453},
+        {-3.229, 0},
         {-std::numeric_limits<double>::infinity(), std::numeric_limits<double>::infinity()},
-        {-0.698, 0.698},
-        {-0.349, 1.919},
-        {-3.314, 0.174},
+        {-0.6108, 1.0472},
+        {0, 1.7453},
+        {-3.229, 0},
         {-std::numeric_limits<double>::infinity(), std::numeric_limits<double>::infinity()},
     };
 
